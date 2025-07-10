05fed11b31b8e892466b98cfdd3c0be9681ef7a3
Akai <chauthanhduy1419@gmail.com>
2025-07-10 11:08:46 +0700

    add resource

--- host/declarations.d.ts
+++ host/declarations.d.ts
@@ -3,3 +3,15 @@ declare module 'productApp/HomePage' {
     const Component: FC;
     export default Component;
 }
+
+declare module 'productApp/CategoryPage' {
+    import { FC } from 'react';
+    const Component: FC;
+    export default Component;
+}
+
+declare module 'productApp/ProductPage' {
+    import { FC } from 'react';
+    const Component: FC;
+    export default Component;
+}
