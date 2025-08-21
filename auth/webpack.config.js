01064aca12b856616dae497a8e07a6ef3abf56c4
Akai <chauthanhduy1419@gmail.com>
2025-08-21 15:43:00 +0700

    update webpack

--- auth/webpack.config.js
+++ auth/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     output: {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: "https://auth-ali.vercel.app/",
+        publicPath: "/",
         clean: true,
     },
     module: {
