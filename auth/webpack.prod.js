8feec7ec83769ce5eebeea1fbd03b6cb63d831f2
Akai <chauthanhduy1419@gmail.com>
2025-08-22 11:40:27 +0700

    update build script

---
+++ auth/webpack.prod.js
@@ -0,0 +1,90 @@
+const path = require("path");
+const { ModuleFederationPlugin } = require("webpack").container;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.tsx",
+    experiments: {
+        topLevelAwait: true,
+    },
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/",
+        clean: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: ["@svgr/webpack"],
+            },
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "authApp",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./LoginPage": "./src/components/Login.tsx",
+                "./RegistrationPage": "./src/components/Registration.tsx",
+            },
+            shared: {
+                react: { singleton: true, eager: true, requiredVersion: false },
+                "react-dom": {
+                    singleton: true,
+                    eager: true,
+                    requiredVersion: false,
+                },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+        new webpack.DefinePlugin({
+            "process.env.FIREBASE_API_KEY": JSON.stringify(
+                process.env.FIREBASE_API_KEY
+            ),
+            "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+                process.env.FIREBASE_AUTH_DOMAIN
+            ),
+            "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+                process.env.FIREBASE_PROJECT_ID
+            ),
+            "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+                process.env.FIREBASE_STORAGE_BUCKET
+            ),
+            "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+                process.env.FIREBASE_MESSAGING_SENDER_ID
+            ),
+            "process.env.FIREBASE_APP_ID": JSON.stringify(
+                process.env.FIREBASE_APP_ID
+            ),
+            "process.env.FIREBASE_MEASUREMENT_ID": JSON.stringify(
+                process.env.FIREBASE_MEASUREMENT_ID
+            ),
+        }),
+    ],
+};
