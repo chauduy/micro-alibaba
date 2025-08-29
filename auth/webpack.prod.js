2dd0b84404a07fdac38b1a67c163609f1c92b929
Akai <chauthanhduy1419@gmail.com>
2025-08-29 10:15:40 +0700

    update webpack

--- auth/webpack.prod.js
+++ auth/webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = {
     output: {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: "https://auth-ali.onrender.com/",
+        publicPath: "https://micro-alibaba.onrender.com/",
         clean: true,
     },
     module: {
