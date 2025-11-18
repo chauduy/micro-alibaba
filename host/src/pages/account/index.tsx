bd68f9be24f96e9662a6d9fd61e0cd4030988d60
Akai <chauthanhduy1419@gmail.com>
2025-11-18 16:32:41 +0700

    connect to account module

--- host/src/pages/account/index.tsx
+++ host/src/pages/account/index.tsx
@@ -2,7 +2,7 @@
 export default function AccountPage() {
     return (
         <iframe
-            src="http://localhost:4300/"
+            src="https://micro-alibaba-o6fe.vercel.app/"
             style={{ width: '100%', height: '100vh', border: 'none' }}
         />
     );
