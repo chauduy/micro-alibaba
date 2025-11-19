9b0f36c1be3ed657950b5f1e014e11c8104b242c
Akai <chauthanhduy1419@gmail.com>
2025-11-19 11:17:59 +0700

    test build

--- host/src/components/layout.tsx
+++ host/src/components/layout.tsx
@@ -1,38 +1,34 @@
-import { Metadata } from 'next';
+import Head from 'next/head';
 
 import AppRedirect from './AppRedirectContext';
 import Footer from './Footer';
 import Header from './Header';
 import { Toaster } from './ui/sonner';
 
-export const metadata: Metadata = {
-    title: 'Ali | Buy anything from anywhere',
-    icons: '/images/brand-logo.png',
-    keywords: ['online marketplace', 'global trade', 'wholesale', 'b2b ecommerce'],
-    robots: 'index, follow',
-    description:
-        'Find quality Manufacturers, Suppliers, Exporters, Importers, Buyers, Wholesalers, Products and Trade Leads from our award-winning International Trade Site. Import & Export on alibaba.com',
-    openGraph: {
-        title: 'Ali | Buy anything in anywhere',
-        description:
-            'Find quality Manufacturers, Suppliers, Exporters, Importers, Buyers, Wholesalers, Products and Trade Leads from our award-winning International Trade Site. Import & Export on alibaba.com',
-        url: 'https://micro-ali.vercell.app',
-        siteName: 'Ali',
-        images: [
-            {
-                url: 'https://micro-ali.vercell.app/images/brand-logo.png',
-                width: 800,
-                height: 600
-            }
-        ],
-        locale: 'en_US',
-        type: 'website'
-    }
-};
-
 function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Head>
+                <title>Ali | Buy anything in anywhere</title>
+                <meta
+                    name="description"
+                    content="Find quality Manufacturers, Suppliers, Exporters, Importers, Buyers, Wholesalers, Products and Trade Leads from our award-winning International Trade Site. Import & Export on alibaba.com"
+                />
+                <meta
+                    name="keywords"
+                    content="online marketplace, global trade, wholesale, b2b ecommerce"
+                />
+                <link
+                    rel="icon"
+                    href="/images/brand-logo.png"
+                    type="image/png"
+                />
+                <link
+                    rel="shortcut icon"
+                    href="/images/brand-logo.png"
+                    type="image/png"
+                />
+            </Head>
             <AppRedirect>
                 <Header />
                 {children}
