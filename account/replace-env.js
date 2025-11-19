e53724b1008eee3ad98307d3096cd4edab2dd8e7
Akai <chauthanhduy1419@gmail.com>
2025-11-19 10:24:11 +0700

    test build

--- account/replace-env.js
+++ account/replace-env.js
@@ -1,17 +1,25 @@
-// replace-env.js
 const fs = require('fs');
+
 const envFilePath = 'src/environments/environment.prod.ts';
 let envContent = fs.readFileSync(envFilePath, 'utf8');
 
-Object.keys(process.env).forEach((key) => {
-    const placeholder = `__${key}__`;
-    if (envContent.includes(placeholder)) {
-        envContent = envContent.replaceAll(placeholder, process.env[key]);
+const replacements = {
+    __API_KEY__: process.env.API_KEY,
+    __AUTH_DOMAIN__: process.env.AUTH_DOMAIN,
+    __PROJECT_ID__: process.env.PROJECT_ID,
+    __STORAGE_BUCKET__: process.env.STORAGE_BUCKET,
+    __MESSAGING_SENDER_ID__: process.env.MESSAGING_SENDER_ID,
+    __APP_ID__: process.env.APP_ID,
+    __MEASUREMENT_ID__: process.env.MEASUREMENT_ID,
+};
+
+Object.entries(replacements).forEach(([placeholder, value]) => {
+    if (value && envContent.includes(placeholder)) {
+        envContent = envContent.replaceAll(placeholder, value);
     }
 });
-console.log('process', process.env);
+
 console.log('envContent', envContent);
-console.log('envFilePath', envFilePath);
 
 fs.writeFileSync(envFilePath, envContent);
-console.log('Environment variables replaced');
+console.log('Done replacing env vars.');
--- account/src/app/store/user.store.ts
+++ account/src/app/store/user.store.ts
@@ -4,21 +4,22 @@ import { User } from '../type';
 
 @Injectable({ providedIn: 'root' })
 export class UserStore {
-  private readonly _user$ = new BehaviorSubject<User | null>(null);
-  readonly user$ = this._user$.asObservable();
-  get user(): User | null {
-    return this._user$.value;
-  }
-
-  setUser(user: User) {
-    this._user$.next(user);
-  }
-
-  async loadUser() {
-    const stored = localStorage.getItem('user');
-    if (stored) {
-      const parseData = JSON.parse(stored);
-      this._user$.next(parseData);
+    private readonly _user$ = new BehaviorSubject<User | null>(null);
+    readonly user$ = this._user$.asObservable();
+    get user(): User | null {
+        return this._user$.value;
     }
-  }
+
+    setUser(user: User) {
+        this._user$.next(user);
+    }
+
+    async loadUser() {
+        console.log('load user');
+        const stored = localStorage.getItem('user');
+        if (stored) {
+            const parseData = JSON.parse(stored);
+            this._user$.next(parseData);
+        }
+    }
 }
