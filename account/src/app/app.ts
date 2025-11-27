1eff19406c419c35ed0409b6c414e241b59c7e13
Akai <chauthanhduy1419@gmail.com>
2025-11-27 10:46:14 +0700

    fix some bug & update better UX

--- account/src/app/app.ts
+++ account/src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, effect, EventEmitter, HostListener, OnInit } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, effect, HostListener, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
@@ -9,8 +9,7 @@ import { Loading } from './components/loading/loading';
 import { UserStore } from './store/user.store';
 import { FavoriteStore } from './store/favorite.store';
 import { OrderStore } from './store/order.store';
-import { auth, db } from 'lib/firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { filter } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -28,7 +27,7 @@ import { doc, getDoc } from 'firebase/firestore';
     styleUrl: './app.scss',
 })
 export class App implements OnInit {
-    isCollapsed = false;
+    isCollapsed = true;
     isMobile = false;
     favoriteList;
     orderList;
@@ -37,17 +36,32 @@ export class App implements OnInit {
     constructor(
         private userStore: UserStore,
         private favoriteStore: FavoriteStore,
-        private orderStore: OrderStore
+        private orderStore: OrderStore,
+        router: Router
     ) {
         this.favoriteList = toSignal(this.favoriteStore.favoriteList$, { initialValue: null });
         this.orderList = toSignal(this.orderStore.orderList$, { initialValue: null });
         this.user = toSignal(this.userStore.user$, { initialValue: null });
+        const navigation = toSignal(router.events.pipe(filter((e) => e instanceof NavigationEnd)), {
+            initialValue: null,
+        });
 
         effect(() => {
             const u = this.user();
             if (u?.uid) {
                 this.favoriteStore.loadFavoriteList(u.uid);
                 this.orderStore.loadOrderList(u.uid);
+            }
+        });
+
+        effect(() => {
+            navigation();
+            const top = document.getElementById('top-logo');
+            if (this.isMobile && !this.isCollapsed) {
+                this.isCollapsed = true;
+            }
+            if (top && this.isMobile) {
+                top.scrollIntoView({ behavior: 'instant' });
             }
         });
     }
--- account/src/app/pages/my-list/my-list.ts
+++ account/src/app/pages/my-list/my-list.ts
@@ -4,6 +4,7 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { CartItem } from '@app/components/cart-item/cart-item';
 import { InspirationList } from '@app/components/inspiration-list/inspiration-list';
 import { FavoriteStore } from '@app/store/favorite.store';
+import { UserStore } from '@app/store/user.store';
 import { Product } from '@app/type';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 import { paging } from 'utils';
@@ -17,12 +18,14 @@ import { paging } from 'utils';
 })
 export class MyList {
     favoriteList;
+    user;
     pagingData = signal<{ totalPage: number; data: any } | null>(null);
     currentPage = signal<number>(1);
     currentData = signal<Product[]>([]);
 
-    constructor(private favoriteStore: FavoriteStore) {
+    constructor(private favoriteStore: FavoriteStore, private userStore: UserStore) {
         this.favoriteList = toSignal(this.favoriteStore.favoriteList$, { initialValue: null });
+        this.user = toSignal(this.userStore.user$, { initialValue: null });
         effect(() => {
             const temp = this.favoriteList();
             if (temp && temp.length > 0) {
@@ -36,11 +39,15 @@ export class MyList {
         effect(() => {
             const page = this.currentPage();
             const pagingData = this.pagingData();
+            const topPoint = document.getElementById('top-point');
             if (pagingData && pagingData.data) {
                 const pageData = pagingData.data[page];
                 if (pageData) {
                     this.currentData.set(pageData);
                 }
+            }
+            if (topPoint) {
+                topPoint.scrollIntoView({ behavior: 'instant' });
             }
         });
     }
@@ -50,7 +57,7 @@ export class MyList {
     }
 
     onRemoveFromList(id: number) {
-        const user = JSON.parse(localStorage.getItem('user')!);
+        const user = this.user();
         if (user) {
             this.favoriteStore.removeFromList(id, user.uid);
         }
